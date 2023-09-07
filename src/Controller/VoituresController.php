1d0fad324e4ae8ebd92940b470c6a69ca93b265a
Aymerik DIEBOLD <aymerik.diebold@otodoo.com>
2023-09-08 01:24:08 +0200

    Ajout de la page Voiture

--- src/Controller/VoituresController.php
+++ src/Controller/VoituresController.php
@@ -6,19 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\VoitureRepository;
+use App\Entity\Voiture;
 
 class VoituresController extends AbstractController
 {
+
+    public function __construct(
+        private VoitureRepository $voitureRepository,
+    )
+    {
+
+    }
+
     /**
      * Page d'accueil, listant les voitures
      */
     #[Route('/', name: 'app_home')]
-    public function index(VoitureRepository $voitureRepository): Response
+    public function index(): Response
     {
-        $voitures = $voitureRepository->findAll();
+        $voitures = $this->voitureRepository->findAll();
 
         return $this->render('accueil.html.twig', [
             'voitures' => $voitures,
+        ]);
+    }
+
+    /**
+     * Page de détail d'une voiture
+     */
+    #[Route('/voiture/{id}', name: 'app_car')]
+    public function voiture(int $id): Response
+    {
+        $voiture = $this->voitureRepository->find($id);
+
+        if(!$voiture) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('voiture.html.twig', [
+            'voiture' => $voiture,
         ]);
     }
 }
